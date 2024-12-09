6089efa6cc3c17b382e8a26c3509248eafd61a28
sanjana782 <121682261+sanjana782@users.noreply.github.com>
2024-12-09 09:44:11 +0530

    student.models.js

--- Backend/src/models/student.models.js
+++ Backend/src/models/student.models.js
@@ -1,37 +1,46 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const StudentSchema = new mongoose.Schema(
-   {
-      usn:{
-        type: String,
-        required: [true, "USN is required"],
-        unique: true
-      },
-      name:{
-        type: String,
-        required: [true, "Name is required"],
-        unique: true
-      },
-      department:{
-        type: String,
-        required: [true, "Department is required"]
-      },
-     sem: {
-       type: Number,
-     },
-      email: {
-        type: String,
-        required: [true, "email is required"],
-        unique: true,
-        lowercase: true
-       },
-       password: {
-         type: String,
-         required: [true, "password is required"],
-         minlength: [8,'Password must contain atleast 8 characters']
-       },
-       {timestamps: true}
+  {
+    usn: {
+      type: String,
+      required: [true, 'USN is required'],
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      unique: true,
+    },
+    department: {
+      type: String,
+      required: [true, 'Department is required'],
+    },
+    sem: {
+      type: Number,
+      min: 1,
+      max: 8,
+    },
+    email: {
+      type: String,
+      required: [true, 'email is required'],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'password is required'],
+      min: [8, 'Password must contain atleast 8 characters'],
+    },
+    phone: {
+      type: String,
+      required: [true, 'Phone number is required'],
+      unique: true,
+      minlength: 10,
+      maxlength: 10,
+    },
+  },
+  { timestamps: true }
+);
 
-  );
-
-export const Student = mongoose.model("Student_Details", StudentSchema);
+export const Student_Details = mongoose.model('Student_Details', StudentSchema);
