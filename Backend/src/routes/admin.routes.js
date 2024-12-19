de2f050ebaacc0432ad60669d560ad1596de7daf
aashish059 <hrihs66@gmail.com>
2024-12-19 19:48:40 +0530

    usn assign

--- Backend/src/routes/admin.routes.js
+++ Backend/src/routes/admin.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { addSubject } from "../controllers/admin/subject.controllers.js";
 import { verifyAdmin } from "../middlewares/auth.middlewares.js";
+import assignUSN from "../controllers/admin/usnAssign.controllers.js";
+import getAllStudentsWithUSN from "../controllers/admin/returnStudentList.controllers.js";
 import { loginAdmin, logoutAdmin, registerAdmin } from "../controllers/admin/auth.controllers.js";
-
 
 const adminRouter = Router();
 
@@ -10,5 +11,7 @@ adminRouter.route("/subject").post(verifyAdmin, addSubject )
 adminRouter.route("/register").post( registerAdmin )
 adminRouter.route("/login").get( loginAdmin )
 adminRouter.route("/logout").get( logoutAdmin )
+adminRouter.route("/assignUSN").post(assignUSN)
+adminRouter.route("/students").get(getAllStudentsWithUSN)
 
 export default adminRouter;
